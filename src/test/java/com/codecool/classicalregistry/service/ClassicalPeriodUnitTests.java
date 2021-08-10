a67331269a9b37a3896f9c9f22bae6a2b1aaf275
valamokka <a476220A>
2021-08-10 12:19:56 +0200

    Optimize unit tests

--- src/test/java/com/codecool/classicalregistry/service/ClassicalPeriodUnitTests.java
+++ src/test/java/com/codecool/classicalregistry/service/ClassicalPeriodUnitTests.java
@@ -3,7 +3,6 @@ package com.codecool.classicalregistry.service;
 
 import com.codecool.classicalregistry.controller.ClassicalPeriodController;
 import com.codecool.classicalregistry.exceptions.DatabaseIsEmptyException;
-import com.codecool.classicalregistry.model.ClassicalPeriod;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -12,12 +11,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import java.sql.SQLIntegrityConstraintViolationException;
-import java.util.Collections;
 
 
 @WebMvcTest(ClassicalPeriodController.class)
@@ -29,19 +24,12 @@ public class ClassicalPeriodUnitTests {
     @MockBean
     ClassicalPeriodService classicalPeriodService;
 
-    ClassicalPeriod CP_1 = new ClassicalPeriod(1L, "Renaissance", 1400, 1600, Collections.emptyList());
-    ClassicalPeriod CP_2 = new ClassicalPeriod(2L, "Baroque", 1600, 1750, Collections.emptyList());
-    ClassicalPeriod CP_3 = new ClassicalPeriod(3L, "Classical", 1750, 1830, Collections.emptyList());
-
     @Test
-    public void getAllPeriods_whenEmptyThrowsException () throws Exception {
+    public void getAllPeriods_whenEmptyThrowsException() throws Exception {
         Mockito.when(classicalPeriodService.listAllClassicalPeriods()).thenThrow(new DatabaseIsEmptyException());
 
-        mockMvc.perform(MockMvcRequestBuilders.
-                        get("/period")).
+        mockMvc.perform(MockMvcRequestBuilders.get("/period")).
                 andExpect(mvcResult -> Assertions.assertTrue(mvcResult.getResolvedException() instanceof DatabaseIsEmptyException));
-
-
     }
 
     @Test
